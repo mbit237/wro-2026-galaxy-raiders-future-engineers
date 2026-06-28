166306c41c686acb89eaa0d87f0b566689954353
mbit237 <mbit237@gmail.com>
2026-06-28 09:14:58 +0800

    Point Cloud Localisation

---
+++ point_cloud_localisation.py
@@ -0,0 +1,103 @@
+import math 
+import numpy as np
+
+from utilities import dot
+
+PERPENDICULAR_DIST_THRESHOLD = 100
+
+walls = [
+    # outer walls, clockwise dir
+    [[0, 0], [0, 3000]], 
+    [[0, 3000], [3000, 3000]], 
+    [[3000, 3000], [3000, 0]], 
+    [[3000, 0], [0, 0]], 
+
+    # inner walls, clockwise dir
+    [[1000, 1000], [1000, 2000]], 
+    [[1000, 2000], [2000, 2000]], 
+    [[2000, 2000], [2000, 1000]], 
+    [[2000, 1000], [1000, 1000]]
+]
+
+def augment_wall(wall):
+    dx = wall[1][0] - wall[0][0]
+    dy = wall[1][1] - wall[0][1]
+    
+    dist = math.sqrt(dx**2 + dy**2)
+    path_vec = [dx, dy]
+    path_dir = math.atan2(dy, dx) * 180 / math.pi # in degrees
+    unit_path_vec = [path_vec[0] / dist, path_vec[1] / dist]
+    p_unit_path_vec = [-1 * unit_path_vec[1], unit_path_vec[0]] 
+    
+    wall.append(path_vec)
+    wall.append(path_dir)
+    wall.append(unit_path_vec)
+    wall.append(p_unit_path_vec)
+    return wall 
+
+def augment_walls(walls):
+    for w in range(len(walls)):
+        walls[w] = augment_wall(walls[w])
+    return walls
+
+walls = augment_walls(walls)
+
+def add_cartesian(pose, lidar_readings):# lidar_readings in cartesian coordinates
+    c_lidar_readings = []
+    for lidar_reading in lidar_readings:
+        distance = lidar_reading[1]
+        theta = (pose[2] + lidar_reading[0]) * math.pi / 180 # in radians, robot angle + lidar's spike angle
+        dx = distance * math.cos(theta)
+        dy = distance * math.sin(theta)
+    
+        x = pose[0] + dx
+        y = pose[1] + dy
+        c_lidar_readings.append([x, y, lidar_reading[0], distance])
+        
+    return c_lidar_readings
+
+def calc_pose(Tx, Ty, theta, odometry_pose):
+    new_x = odometry_pose[0] + Tx[0]
+    new_y = odometry_pose[1] + Ty[1]
+    new_angle = odometry_pose[2] + theta
+
+    return [new_x, new_y, new_angle]
+
+def localise(odometry_pose, sensor_readings):
+    if not sensor_readings["lidar"]:
+        return False
+    
+    matrix_A = []
+    matrix_B = []
+    
+    c_lidar_readings = add_cartesian(odometry_pose, sensor_readings["lidar"])
+    for c_lidar_reading in c_lidar_readings:
+        for wall in walls:
+            x1 = c_lidar_reading[0]
+            y1 = c_lidar_reading[1]
+
+            lidar_reading_from_wall_start_vec = [c_lidar_reading[0] - wall[0][0], c_lidar_reading[1] - wall[0][1]]
+            perpendicular_dist_from_wall = dot(wall[5], lidar_reading_from_wall_start_vec) # p_unit_vec * vec_start_from_lidar_point
+
+            # Found match
+            if abs(perpendicular_dist_from_wall) <= PERPENDICULAR_DIST_THRESHOLD:
+                if wall[3] == 0 or wall[3] == 180: # wall direction vertical
+                    matrix_A.append([-y1, 1, 0])
+                    matrix_B.append([perpendicular_dist_from_wall])
+
+                elif wall[3] == 90 or wall[3] == 270: # wall direction horizontal
+                    matrix_A.append([x1, 0, 1])
+                    matrix_B.append([perpendicular_dist_from_wall])
+
+    matrix_A = np.matrix(matrix_A)
+    matrix_B = np.matrix(matrix_B)
+
+    psuedoinverse_A = np.linalg.pinv(matrix_A)
+    matrix_x = np.matmul(psuedoinverse_A, matrix_B)
+    
+    Tx = matrix_x[0][0]
+    Ty = matrix_x[1][0]
+    theta = matrix_x[2][0] * 180 / (2 * math.pi)
+    point_cloud_pose = calc_pose(Tx, Ty, theta, odometry_pose)
+
+    return point_cloud_pose
--- utilities.py
+++ utilities.py
@@ -4,3 +4,6 @@ def dir_to_point(pose, point):
     # given a pose and x, y coordinate, return a direction relative to the robot, ie. 0 is straight ahead
     angle_uncorrected = math.atan2(point[1] - pose[1], point[0] - pose[0])
     return angle_uncorrected - pose[2]
+
+def dot(v1, v2):
+    return v1[0] * v2[0] + v1[1] * v2[1]
